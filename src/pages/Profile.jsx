057dc36480f0bb60ae18ccf98d8eefd4f64e6140
zntb <zntbdev@proton.me>
2024-02-21 15:19:38 +0200

    Add User Sign In Functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,17 @@
+import { useEffect, useState } from 'react';
+import { getAuth } from 'firebase/auth';
+
 function Profile() {
-  return <div>Profile</div>;
+  const [user, setUser] = useState(null);
+
+  const auth = getAuth();
+
+  useEffect(() => {
+    console.log(auth.currentUser);
+    setUser(auth.currentUser);
+  }, []);
+
+  return user ? <h1>{user.displayName}</h1> : <p>Not logged in!</p>;
 }
 
 export default Profile;
