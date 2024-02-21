5f44bce15f17c7724f2484e2b7f67de131d3a2e7
zntb <zenetibi@yahoo.com>
2024-02-21 09:30:55 +0200

    Add Pages & Routes

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: 'house-marketplace-f6cc8.firebaseapp.com',
+  projectId: 'house-marketplace-f6cc8',
+  storageBucket: 'house-marketplace-f6cc8.appspot.com',
+  messagingSenderId: '80838074061',
+  appId: '1:80838074061:web:a863af77245727ca2b5384',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
