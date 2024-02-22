747c1ed13c682b1aeadcc76e7eda3edf0120d51c
zntb <zntbdev@proton.me>
2024-02-22 08:39:54 +0200

    Fetch Listings From Firebase

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -23,7 +23,7 @@ const Explore = () => {
             />
             <p className="exploreCategoryName">Places for rent</p>
           </Link>
-          <Link to="/category/sell">
+          <Link to="/category/sale">
             <img
               src={sellCategoryImage}
               alt="sell"
