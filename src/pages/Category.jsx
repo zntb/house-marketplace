b03ff8a7f5d3d7aff3b7b92a47b661ddc74daa56
zntb <zenetibi@yahoo.com>
2024-02-22 10:43:01 +0200

    Add Listing Item Component

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -12,7 +12,7 @@ import {
 import { db } from '../firebase.config';
 import { toast } from 'react-toastify';
 import Spinner from '../components/Spinner';
-// import ListingItem from '../components/ListingItem';
+import ListingItem from '../components/ListingItem';
 
 function Category() {
   const [listings, setListings] = useState(null);
@@ -73,7 +73,11 @@ function Category() {
           <main>
             <ul className="categoryListings">
               {listings.map((listing) => (
-                <h3 key={listing.id}>{listing.data.name}</h3>
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                />
               ))}
             </ul>
           </main>
