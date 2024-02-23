2116c4dde08fd17516d01ac1af19abb18499fdc4
zntb <zntbdev@proton.me>
2024-02-23 11:24:40 +0200

    Add Load More Pagination Functionality

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -64,7 +64,7 @@ function SignUp() {
     <>
       <div className="pageContainer">
         <header>
-          <p className="pageHeader">Welcome Back!</p>
+          <p className="pageHeader">Welcome!</p>
         </header>
 
         <form onSubmit={onSubmit}>
